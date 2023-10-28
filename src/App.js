3a93502ee10c01ff91daddd01963b9d405702b02
Divy Parekh <divyparekh1810@gmail.com>
2023-10-28 18:28:22 +0530

    added home component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import SignIn from './components/signIn/SignIn'
 import AddProduct from './components/add-products/AddProduct'
 import SignUp from './components/signUp/SignUp'
 import { Route, Routes } from 'react-router-dom'
+import Home from './components/home/Home'
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<SignIn />} />
       <Route path="/AddProduct" element={<AddProduct />} />
       <Route path="/SignUp" element={<SignUp />} />
     </Routes>
